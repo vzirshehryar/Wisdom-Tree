976c50a6cdfc41b9bf4f2505a7196ec3d00da730
Wazir Shehryar Ali <95472169+vzirshehryar@users.noreply.github.com>
2024-05-10 21:16:39 +0500

    Update OxAuth.tsx for new client id

--- components/OxAuth.tsx
+++ components/OxAuth.tsx
@@ -12,7 +12,7 @@ const OxAuth = () => {
         primaryColor="#121713"
         companyName="WisdomTree"
         logoUrl="https://www.wisdomtree.com/investments/-/media/logo.svg?iar=0&hash=D72308FE097419956FDC89213558FC39"
-        clientId="663a12cfc3725258e208c9fb"
+        clientId="663e44cc760f350c8d4bfe7c"
         claimsRequired={[CLAIMS.Identity, CLAIMS.AccreditedInvestor]}
         onLogin={(user: any) => {
           console.log("helo ducky", user);
